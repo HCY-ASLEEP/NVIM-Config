88fb87441f96a41f8a3b33b0afd73de029fe86d7
HCY-ASLEEP <76894615+HCY-ASLEEP@users.noreply.github.com>
2023-03-09 19:54:34 +0800

    Update coc-nodejs-installer.sh

--- coc-nodejs-installer.sh
+++ coc-nodejs-installer.sh
@@ -4,9 +4,8 @@ sudo curl -L https://npmmirror.com/mirrors/node/v18.15.0/$nodejs_pack -o /opt/$n
 sudo tar -xvf /opt/$nodejs_pack -C /opt/
 sudo mv /opt/$nodejs_version /opt/nodejs
 sudo rm /opt/$nodejs_pack
-sudo sh -c 'echo "\nexport PATH=$PATH:/opt/nodejs/bin/" >> /etc/bash.bashrc'
+sudo sh -c 'echo "\nexport PATH=\$PATH:/opt/nodejs/bin/" >> /etc/bash.bashrc'
 source /etc/bash.bashrc
-npm config set registry https://registry.npm.taobao.org
-/opt/nodejs/bin/npm i -g yarn
+/opt/nodejs/bin/npm config set registry https://registry.npm.taobao.org
 unset nodejs_version
 unset nodejs_pack
