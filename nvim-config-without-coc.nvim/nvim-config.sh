8ee620fdd708429af89afb8ab1c3687ce7faea5f
HCY-ASLEEP <76894615+HCY-ASLEEP@users.noreply.github.com>
2023-03-22 19:22:22 +0800

    Create nvim-config.sh

---
+++ nvim-config-without-coc.nvim/nvim-config.sh
@@ -0,0 +1,12 @@
+echo "  vim-plug download start"
+echo
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo
+echo "  init.vim download start"
+echo
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/init.vim --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/nvim-config-without-coc.nvim/init.vim'
+echo
+echo "  markdown.vim download start"
+echo
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/markdown.vim --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/nvim-config-without-coc.nvim/markdown.vim'
+echo
