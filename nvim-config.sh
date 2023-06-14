388cd97d3dacb51e4d8cf976524307ebbd14e6be
HCY-ASLEEP <76894615+HCY-ASLEEP@users.noreply.github.com>
2023-06-14 15:03:13 +0800

    Update nvim-config.sh

--- nvim-config.sh
+++ nvim-config.sh
@@ -6,10 +6,11 @@ echo "  init.vim download start"
 echo
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/init.vim --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/init.vim'
 echo
+echo "  lsp.lua download start"
+echo
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/lsp.lua --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/lsp.lua'
+echo
 echo "  markdown.vim download start"
 echo
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/markdown.vim --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/markdown.vim'
 echo
-echo "  coc-settings.json download start"
-echo
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/coc-settings.json --create-dirs https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/coc-settings.json'
