c68108bc85f20cff3ceee02c962d168cef657ab7
HCY-ASLEEP <2420066864@qq.com>
2023-12-31 13:50:14 +0000

    Update nodejs-installer.sh

--- nodejs-installer.sh
+++ nodejs-installer.sh
@@ -1,13 +1,21 @@
-nodejs_version='node-v18.15.0-linux-x64'
-nodejs_pack=$nodejs_version".tar.xz"
-sudo curl -L https://npmmirror.com/mirrors/node/v18.15.0/$nodejs_pack -o /opt/$nodejs_pack
-sudo tar -xvf /opt/$nodejs_pack -C /opt/
-sudo mv /opt/$nodejs_version /opt/nodejs
-sudo rm /opt/$nodejs_pack
-sudo sh -c 'echo "\nexport PATH=\$PATH:/opt/nodejs/bin/" >> /etc/bash.bashrc'
+cd ~
+latest_version=$(curl -sL https://nodejs.org/en/download/ | grep -o -E 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
+download_url="https://nodejs.org/dist/$latest_version/node-$latest_version-linux-x64.tar.xz"
+install_dir="/opt/nodejs"
+sudo mkdir $install_dir
+sudo chown -R devenv $install_dir
+sudo chgrp -R devenv $install_dir
+curl -o nodejs.tar.xz $download_url
+tar -xf nodejs.tar.xz -C $install_dir
+sudo sh -c 'echo "\nexport PATH=\$PATH:\$install_dir/node-\$latest_version-linux-x64/bin/" >> /etc/bash.bashrc'
+rm nodejs.tar.xz
 . /etc/bash.bashrc
-export PATH=$PATH:/opt/nodejs/bin/
+export PATH=$PATH:$install_dir/node-$latest_version-linux-x64/bin/
+npm config set registry https://registry.npm.taobao.org
 npm config set registry https://registry.npm.taobao.org
 npm i -g yarn
-unset nodejs_version
-unset nodejs_pack
+unset latest_version
+unset download_url
+unset install_dir
+bash
+echo "---- Node.js $latest_version installed ----"
