dd71140d4206e7ecc7c9457918e0bc6f55fcd1a6
HCY-ASLEEP <2420066864@qq.com>
2024-01-27 16:02:59 +0800

    Update vim-config-without-lsp.sh

--- vim-config-without-lsp.sh
+++ vim-config-without-lsp.sh
@@ -1,13 +1,18 @@
-dir_path="~/.config/nvim/"
+dir_path=$HOME"/.config/nvim/"
 if [ -d "$dir_path" ]; then
   echo ""
   echo "Directory $dir_path exists"
   echo "----- ABORT -----"
   echo "You can backup $dir_path and remove $dir_path , then try again"
   echo ""
-  exit 0
+  unset dir_path
+  exit 1
 fi
 curl -sL https://raw.githubusercontent.com/HCY-ASLEEP/NVIM-Config/main/nvim-config-without-lsp/nvim-config.sh | sh
+dir_path=$HOME"/vim/"
+if [ -d "$dir_path" ]; then
+    rm -r ~/vim/
+fi
 mv ~/.config/nvim/* ~
 rm -r ~/.config/nvim/
 mv ~/init.vim ~/.vimrc
