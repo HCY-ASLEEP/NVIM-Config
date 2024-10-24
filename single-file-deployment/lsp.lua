e1d9bf2bb5116c2dbd194de29c9f0d12cdd55502
HCY-ASLEEP <2420066864@qq.com>
2024-10-24 11:24:21 +0800

    Maybe sometimes we need to try other great tools and then convert them into self step by step

--- single-file-deployment/lsp.lua
+++ single-file-deployment/lsp.lua
@@ -861,4 +861,32 @@ vim.api.nvim_create_user_command("OpenSymbolOutlineSorted", function()
     activate_sorted_view(0)
 end, {})
 
+-- -- 1. 准备lazy.nvim模块（存在性检测）
+-- -- stdpath("data")
+-- -- macOS/Linux: ~/.local/share/nvim
+-- -- Windows: ~/AppData/Local/nvim-data
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+-- 	vim.fn.system({
+-- 		"git",
+-- 		"clone",
+-- 		"--filter=blob:none",
+-- 		"https://gitee.com/hcy-asleep/lazy.nvim.git",
+-- 		"--branch=stable", -- latest stable release
+-- 		lazypath,
+-- 	})
+-- end
+-- -- 
+-- -- 2. 将 lazypath 设置为运行时路径
+-- -- rtp（runtime path）
+-- -- nvim进行路径搜索的时候，除已有的路径，还会从prepend的路径中查找
+-- -- 否则，下面 require("lazy") 是找不到的
+-- vim.opt.rtp:prepend(lazypath)
+-- 
+-- -- 3. 加载lazy.nvim模块
+-- require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+
+
+vim.opt.runtimepath:append("/root/nvim-treesitter/")
+
 
